d00c063924dde74a007ffaa1323739c665156bc4
eda oncel <edaoncel@email.com>
2023-05-19 14:40:47 +0300

    Sayi Tahmin Oyunu Projesi

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace sayi_tahmini_oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblSayi = new System.Windows.Forms.Label();
+            this.lblMesaj = new System.Windows.Forms.Label();
+            this.btnTahminEt = new System.Windows.Forms.Button();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.btnKapat = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txt_tahmin = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblSayi
+            // 
+            this.lblSayi.AutoSize = true;
+            this.lblSayi.Location = new System.Drawing.Point(227, 60);
+            this.lblSayi.Name = "lblSayi";
+            this.lblSayi.Size = new System.Drawing.Size(90, 20);
+            this.lblSayi.TabIndex = 0;
+            this.lblSayi.Text = "bir sayi girin";
+            // 
+            // lblMesaj
+            // 
+            this.lblMesaj.AutoSize = true;
+            this.lblMesaj.Location = new System.Drawing.Point(264, 145);
+            this.lblMesaj.Name = "lblMesaj";
+            this.lblMesaj.Size = new System.Drawing.Size(114, 20);
+            this.lblMesaj.TabIndex = 1;
+            this.lblMesaj.Text = "1-100 sayi girin";
+            // 
+            // btnTahminEt
+            // 
+            this.btnTahminEt.Location = new System.Drawing.Point(486, 97);
+            this.btnTahminEt.Name = "btnTahminEt";
+            this.btnTahminEt.Size = new System.Drawing.Size(151, 51);
+            this.btnTahminEt.TabIndex = 2;
+            this.btnTahminEt.Text = "tahmin et";
+            this.btnTahminEt.UseVisualStyleBackColor = true;
+            this.btnTahminEt.Click += new System.EventHandler(this.btnTahminEt_Click);
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.Location = new System.Drawing.Point(255, 199);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(382, 39);
+            this.btnBaslat.TabIndex = 3;
+            this.btnBaslat.Text = "oyunu baslat";
+            this.btnBaslat.UseVisualStyleBackColor = true;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // btnKapat
+            // 
+            this.btnKapat.Location = new System.Drawing.Point(255, 259);
+            this.btnKapat.Name = "btnKapat";
+            this.btnKapat.Size = new System.Drawing.Size(382, 38);
+            this.btnKapat.TabIndex = 4;
+            this.btnKapat.Text = "oyunu bitir";
+            this.btnKapat.UseVisualStyleBackColor = true;
+            this.btnKapat.Click += new System.EventHandler(this.btnKapat_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 5;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txt_tahmin
+            // 
+            this.txt_tahmin.Location = new System.Drawing.Point(366, 64);
+            this.txt_tahmin.Name = "txt_tahmin";
+            this.txt_tahmin.Size = new System.Drawing.Size(178, 26);
+            this.txt_tahmin.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1169, 450);
+            this.Controls.Add(this.txt_tahmin);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnKapat);
+            this.Controls.Add(this.btnBaslat);
+            this.Controls.Add(this.btnTahminEt);
+            this.Controls.Add(this.lblMesaj);
+            this.Controls.Add(this.lblSayi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSayi;
+        private System.Windows.Forms.Label lblMesaj;
+        private System.Windows.Forms.Button btnTahminEt;
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.Button btnKapat;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txt_tahmin;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayi_tahmini_oyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int sayi;
+        Random rastgele = new Random();
+        private object textSayi;
+       
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+            sayi = rastgele.Next(0, 101);
+        }
+
+        private void btnTahminEt_Click(object sender, EventArgs e)
+        {
+            int girilensayi = Convert.ToInt32(txt_tahmin.Text);
+            if(sayi == girilensayi)
+            {
+                lblMesaj.Text = "daha büyük bir sayi girin";
+            }
+            if (sayi < girilensayi)
+            {
+                lblMesaj.Text = "daha kücük bir sayi girin";
+            }
+
+        }
+
+        private void btnKapat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
